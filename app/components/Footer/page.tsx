25b810822a8e9563dfa700d4bf8b6707e48e28d0
joe-boadi <joeboadi1@gmail.com>
2024-05-17 18:38:15 +0000

    Add footer

---
+++ app/components/Footer/page.tsx
@@ -0,0 +1,27 @@
+import { FaLinkedin, FaSquareFacebook, FaSquareXTwitter } from "react-icons/fa6";
+import Image from 'next/image'
+
+export default function Footer () {
+    return (
+        <footer className="footer p-10 bg-base-300 text-neutral-content bg-opacity-55 backdrop-blur-lg">
+            <aside>
+                <div>
+                    <Image 
+                    src="/RoofScout_Reimagined.png" 
+                    alt="RoofScout Logo" 
+                    width={50} 
+                    height={50}/>
+                </div>
+                <p className="font-extrabold">RoofScout Real Estate Ltd.<br/>Finding you the perfect Home <br />Copyright &copy; 2024</p>
+                </aside> 
+                <nav>
+                <h6 className="footer-title">Social</h6> 
+                <div className="grid grid-flow-col gap-4">
+                    <a href="https://github.com/joe-boadi" title="Facebook"><svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24" className="fill-current font-bold"> <FaSquareFacebook /> </svg></a>
+                    <a href="https://www.linkedin.com/in/kwadjo-boadi-mantey-43b670271/" title="LinkedIn"><svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24" className="fill-current font-bold"> <FaLinkedin /></svg> </a>
+                    <a href="https://www.x.com/joeboadiDev_" title="X/Twitter"><svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24" className="fill-current font-bold"> <FaSquareXTwitter /> </svg></a>
+                </div>
+            </nav>
+      </footer>
+    )
+}
